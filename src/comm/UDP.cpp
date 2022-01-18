c0560ecc210452f1d7b77d896ec96be7f9510294
Frantisek Burian <BuFran@seznam.cz>
2022-01-18 13:01:40 +0100

    feat: UDP Zjednoduseni / code reuse for ockaddr a prevod z a do + pouzit vektor

--- src/comm/UDP.cpp
+++ src/comm/UDP.cpp
@@ -17,6 +17,48 @@
 
 using namespace RoboUtils::COMM;
 
+class SAHelper {
+public:
+    sockaddr_in sa{};
+
+    SAHelper(uint16_t port = 0)
+    {
+        memset(&sa, 0, sizeof(sa));
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = htonl(INADDR_ANY);
+        sa.sin_port = htons(port);
+    }
+
+    SAHelper(const std::string& host)
+     : SAHelper(0)
+    {
+        auto spl = RoboUtils::split(host, '/');
+
+        if (spl.size() != 2)
+            throw std::logic_error("UDP::HOST must have port specified <ip>/<port>");
+
+        if (!std::all_of(spl[1].begin(), spl[1].end(), ::isdigit))
+            throw std::logic_error("UDP::HOST port must be only digits <ip>/<port>");
+
+        if (!inet_pton(AF_INET, spl[0].c_str(), &(sa.sin_addr)))
+            throw std::logic_error("UDP::HOST ip address is not known");
+
+        sa.sin_port = htons(std::stoi(spl[1]));
+    }
+
+    operator std::string()
+    {
+        char str[INET_ADDRSTRLEN];
+
+        inet_ntop(AF_INET, &(sa.sin_addr), str, INET_ADDRSTRLEN);
+
+        std::vector<std::string> sender { str, std::to_string(ntohs(sa.sin_port)) };
+
+        return RoboUtils::join(sender,"/");
+    }
+
+};
+
 UDP::UDP()
 {
     // FIXME wrong api - constructor should not throw !
@@ -31,12 +73,9 @@ UDP::~UDP()
 
 void UDP::bind(uint16_t port)
 {
-    sockaddr_in sa{};
-    memset(&sa, 0, sizeof(sa));
-    sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = htonl(INADDR_ANY);
-    sa.sin_port = htons(port);
-    if (::bind(socketDescriptor, (struct sockaddr *) &sa, sizeof(sa)) < 0)
+    SAHelper sah;
+
+    if (::bind(socketDescriptor, (struct sockaddr *) &sah.sa, sizeof(sah.sa)) < 0)
         throw std::logic_error("Failed to bind UDP packet");
 
     bound = true;
@@ -44,24 +83,12 @@ void UDP::bind(uint16_t port)
 
 void UDP::send(const std::string &host, const uint8_t *buffer, std::size_t size) const
 {
-    auto spl = split(host, '/');
-
     if (!bound)
         throw std::logic_error("UDP is not bound");
 
-    if (spl.size() != 2)
-        throw std::logic_error("UDP::SEND host must have port specified <ip>/<port>");
+    SAHelper sa(host);
 
-    if (!std::all_of(spl[1].begin(), spl[1].end(), ::isdigit))
-        throw std::logic_error("UDP::SEND host port must be only digits <ip>/<port>");
-
-    //inet_pton(AF_INET, "192.0.2.33", &(sa.sin_addr));
-    sockaddr_in ca{};
-    memset(&ca, 0, sizeof(ca));
-    ca.sin_family = AF_INET;
-    ca.sin_addr.s_addr = inet_addr(spl[0].c_str());
-    ca.sin_port = htons(std::stoi(spl[1]));
-    if (sendto(socketDescriptor, buffer, size, 0, (struct sockaddr *) &ca, sizeof(ca)) != size)
+    if (sendto(socketDescriptor, buffer, size, 0, (struct sockaddr *) &sa.sa, sizeof(sa.sa)) != size)
         throw std::logic_error("Failed to send UDP packet  ");
 }
 
@@ -88,25 +115,17 @@ std::tuple<std::string, std::vector<uint8_t>> UDP::receive() const
     if (!bound)
         throw std::logic_error("UDP is not bound");
 
-    uint8_t data[1500];
-    char str[INET_ADDRSTRLEN];
+    SAHelper sa;
+    unsigned int clientlen = sizeof(sa.sa);
+    std::vector<uint8_t> result(1500);
 
-    sockaddr_in ca{};
-    memset(&ca, 0, sizeof(ca));
-    ca.sin_family = AF_INET;
-    unsigned int clientlen = sizeof(ca);
-
-    auto read = recvfrom(socketDescriptor, data, 1500, 0, (struct sockaddr *) &ca, &clientlen);
+    auto read = recvfrom(socketDescriptor, result.data(), result.size(), 0, (struct sockaddr *) &sa.sa, &clientlen);
     if (read < 0)
         throw std::logic_error("Failed to receive UDP packet");
 
-    inet_ntop(AF_INET, &(ca.sin_addr), str, INET_ADDRSTRLEN);
+    result.resize(read);
 
-    std::vector<std::string> sender { str, std::to_string(ntohs(ca.sin_port)) };
-    std::vector<uint8_t> result {data, data + read };
-    std::string send = join(sender,"/");
-
-    return std::tie(send, result);
+    return std::tie(sa, result);
 }
 
 std::tuple<std::string, std::string> UDP::receiveStr() const
