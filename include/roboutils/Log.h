12a46a08b6ea131a5d207a338c87b9dc85f1d487
Frantisek Burian <BuFran@seznam.cz>
2022-01-27 11:24:32 +0100

    feat: Big simplification of Log

--- include/roboutils/Log.h
+++ include/roboutils/Log.h
@@ -1,50 +1,30 @@
-#include <utility>
-
+#pragma once
 //
 // Created by Matous Hybl on 2018-10-12.
 //
 
-#ifndef FIRMWARE_LOG_H
-#define FIRMWARE_LOG_H
-
+#include <utility>
 #include <iostream>
 #include <string>
 #include <functional>
 #include <sstream>
-#include "roboutils/comm/UDP.h"
+
+#include <roboutils/comm/UDP.h>
 
 namespace RoboUtils {
 
-    class Log;
-
-    class LogLine {
-
-
-        friend class Log;
-    public:
-        explicit LogLine(const Log * parent);
-        LogLine(LogLine &line);
-        ~LogLine();
-
-        template<class T>
-        LogLine &operator<<(const T &value)  {
-            buffer_ << value;
-            return *this;
-        }
-
-        LogLine &operator<<(std::ostream &(*manipulator)(std::ostream &));
-
-    private:
-        std::stringstream buffer_{};
-        const Log *parent_{nullptr};
-        std::string time_;
-    };
-
     class Log {
-        friend class LogLine;
+    private:
+        const char * level_{nullptr};
+
+        class LogLine : public std::stringstream {
+        public:
+            explicit LogLine(const Log & parent);
+            LogLine(LogLine &line);
+            ~LogLine() override;
+        };
 
     public:
-        // TODO this should be constexpr in the future ...
         static const Log error;
         static const Log warning;
         static const Log info;
@@ -52,26 +32,23 @@ namespace RoboUtils {
 
         static std::function<void(const std::string &)> writer;
 
-        explicit constexpr Log(const char *level);
+        explicit constexpr Log(const char *level)
+           : level_(level)
+        {
+        }
 
         template<class T>
         LogLine operator<<(const T &value) const {
-            LogLine line(this);
+            LogLine line(*this);
             line << value;
             return line;
         }
 
-        void log(const std::string& message) const;
-        void log(const LogLine& message) const;
+        LogLine line() const {
+            return LogLine(*this);
+        }
 
-    private:
-        const char * level_;
-
-        static std::string time();
     };
 
 
 }
-
-
-#endif //FIRMWARE_LOG_H
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,12 +1,9 @@
 //
 // Created by Matous Hybl on 2018-10-12.
 //
-
-#include <sstream>
 #include <roboutils/Log.h>
 
 using namespace RoboUtils;
-
 
 const Log Log::error = Log("ERROR");
 const Log Log::warning = Log("WARNING");
@@ -14,24 +11,9 @@ const Log Log::info = Log("INFO");
 const Log Log::debug = Log("DEBUG");
 std::function<void(const std::string &)> Log::writer{};
 
-constexpr Log::Log(const char *lvl)
- : level_(lvl)
-{
-}
-
-void Log::log(const std::string& message) const
-{
-    if (writer)
-        writer("[" + std::string(level_) + "]: " + Log::time() + " - " + message);
-}
-
-void Log::log(const LogLine& message) const
-{
-    if (writer)
-        writer("[" + std::string(level_) + "]: " + message.time_ + " - " + message.buffer_.str());
-}
-
-std::string Log::time()
+// gran time in constructor
+Log::LogLine::LogLine(const Log & parent)
+  : std::basic_ios<char>{}, std::stringstream{""}
 {
     time_t rawTime;
     ::time(&rawTime);
@@ -39,42 +21,28 @@ std::string Log::time()
     std::string timeString = ctime(&rawTime);
 
     // remove newline
-    return timeString.substr(0, timeString.size() - 1);
+    timeString.pop_back();
+
+    *this << "[" << parent.level_ << "]:" << timeString << " - ";
 }
 
-// gran time in constructor
-LogLine::LogLine(const Log * parent)
-  : parent_(parent) , time_(Log::time())
+Log::LogLine::LogLine(LogLine &line)
+  : std::basic_ios<char>{}, std::stringstream{line.str()}
 {
-}
-
-LogLine::LogLine(LogLine &line)
-  : parent_(line.parent_), time_(line.time_)
-{
-    buffer_.str(line.buffer_.str());
-    line.buffer_.str(std::string());
+    // delete the original
+    line.str(std::string());
 }
 
 
-LogLine::~LogLine()
+Log::LogLine::~LogLine()
 {
     // flush at destructor in the case it has not been flushed
-    if (!buffer_.str().empty()) {
-        parent_->log(*this);
-        buffer_.str(std::string());
-    }
+    if (str().empty())
+        return;
+
+    auto w = Log::writer;
+    if (w)
+        w(this->str());
+    str(std::string());
+
 }
-
-// flush on endl or flush
-LogLine &LogLine::operator<<(std::ostream &(*manipulator)(std::ostream &))
-{
-    if (manipulator == static_cast<std::ostream &(*)(std::ostream &)>(std::flush) ||
-        manipulator == static_cast<std::ostream &(*)(std::ostream &)>(std::endl)) {
-
-        parent_->log(*this);
-        buffer_.str(std::string());
-    }
-
-    return *this;
-}
-
