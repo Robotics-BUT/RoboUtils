2877b5c943b90b0cd2d515c12cf63d159e870cc5
Frantisek Burian <BuFran@seznam.cz>
2022-02-21 19:09:31 +0100

    feat: Implementace RAII locale - explicit instantiation chybela

--- src/util/locales.cpp
+++ src/util/locales.cpp
@@ -6,7 +6,7 @@ template <int LC>
 Locale<LC>::Locale(const char *loc)
  : loc_{std::setlocale(LC, nullptr)}
 {
-  std::setlocale(LC, "C");
+  std::setlocale(LC, loc);
 }
 
 template <int LC>
@@ -14,3 +14,7 @@ Locale<LC>::~Locale()
 {
   std::setlocale(LC,loc_.c_str());
 }
+
+// Explicit instantiation
+template class RoboUtils::Locale<LC_NUMERIC>;
+template class RoboUtils::Locale<LC_TIME>;
